7e8ecd5003572d21c8b8b8e4ec7119751d803b37
root <root@webby6841.(none)>
2012-02-19 22:54:44 -0500

    renamed

---
+++ index.php
@@ -0,0 +1,55 @@
+<html><head><title>byWaiting.com</title><link rel="stylesheet" href="stylesheets/skeleton.css">
+<link rel="stylesheet" href="stylesheets/app.css"></head>
+<body><div id="horizlogo"></div><div id="home-view"><div class="container">
+<center><div id="horizlogo" class="sixteen columns">
+<img src="images/logo_horiz.png"></div></center>
+</div>
+<div class="container">
+<div id="todo-app">
+<div id="new-todo-view"><div class="container" div="" id="sixteen-columns">
+<div class="one-third column">
+<input id="new-todo" placeholder="Party Size" type="number" maxlength="10">
+</div>
+<div class="one-third column">
+<input id="new-todo" column"="" placeholder="Name" type="number" maxlength="10">
+</div>
+<div class="one-third column">
+<input id="new-todo" column"="" placeholder="Phone Number" type="number" maxlength="10">
+</div>
+<div class="ui-tooltip-top">Press Enter to save this number</div>
+</div></div><div id="todos-view"><div class="container">
+<div id="sixteen-columns">
+<ul id="todos">
+<li><div class="todo">
+  <div class="display">
+    <input class="check" type="checkbox">
+  <div class="todo-content">212-222-0000</div>
+    <span class="todo-destroy"></span>
+  </div>
+  <div class="edit">
+    <input class="todo-input" type="text" value="Across The Stars (Love Theme from Star Wars: Episode II)">
+  </div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div><div id="stats-view">
+  <span class="todo-count">
+    <span class="number">4</span>
+    <span class="word">
+      parties
+    </span>
+    in queue.
+  </span>
+
+
+
+</div></div>
+</div>
+
+</div><div id="holder"><div id="five-columns"></div></div><div id="buildings"></div>
+
+<div id="footer"><p>Built at API Hackday NYC 2012</p></div>
+
+</body></html>
